00cf1daaec318d356cd2842bc6b584367b2b1ef6
Administrator <819609179@qq.com>
2023-11-18 11:27:53 +0800

    第3次

---
+++ 333.py
@@ -0,0 +1,8 @@
+from string import Template
+
+# template = Template("My name is $name and I am $age years old.")
+# message = template.substitute(name="Alice", age=25)
+# print(message)
+
+aa = [1,2,3,4,5,6,7]
+print(aa[3:5])
